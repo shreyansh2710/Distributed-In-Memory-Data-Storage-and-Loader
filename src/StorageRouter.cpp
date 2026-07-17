7c5dde6cf515ce27cfcfb50e3f75c29be48d2f2e
shreyansh2710 <73734393+shreyansh2710@users.noreply.github.com>
2026-07-17 16:32:23 +0530

    Update StorageRouter.cpp

--- src/StorageRouter.cpp
+++ src/StorageRouter.cpp
@@ -1,13 +1,10 @@
+// src/StorageRouter.cpp
 #include "StorageRouter.hpp"
-#include <algorithm>
 #include <functional>
 
-StorageRouter::StorageRouter(const std::vector<int>& node_ids) : cluster_nodes(node_ids) {
-    std::sort(cluster_nodes.begin(), cluster_nodes.end());
-}
+StorageRouter::StorageRouter(int total_nodes) : node_count(total_nodes) {}
 
 int StorageRouter::get_owner_node_id(const std::string& key) const {
-    if (cluster_nodes.empty()) return -1;
     size_t hash_val = std::hash<std::string>{}(key);
-    return cluster_nodes[hash_val % cluster_nodes.size()];
+    return static_cast<int>(hash_val % node_count);
 }
